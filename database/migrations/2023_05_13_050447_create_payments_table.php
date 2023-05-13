275a66f7bb8d6aecf269a401f1db36797c9f6c10
long <tranthoilong.giaitri@gmail.com>
2023-05-13 17:55:47 +0700

    up

---
+++ database/migrations/2023_05_13_050447_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('users_id')->constrained();
+            $table->foreignId('orders_id')->constrained();
+            $table->string('payment_methods');
+            $table->decimal('amount');
+            $table->timestamp('payment_of_day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
